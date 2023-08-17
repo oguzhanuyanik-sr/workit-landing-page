f3d4234c31814a0516a85350cd82c534c8dd7156
oguzhanuyanik-sr <oguzhanuyanik.sr@gmail.com>
2023-08-17 13:24:06 +0300

    project created

---
+++ src/utils/constants.ts
@@ -0,0 +1,34 @@
+import LightLogo from '../assets/logo-light.svg';
+import DarkLogo from '../assets/logo-dark.svg';
+import HeroImage from '../assets/image-hero.webp';
+import LeftPattern from '../assets/bg-pattern-3.svg';
+import RightPattern from '../assets/bg-pattern-2.svg';
+import Founder from '../assets/image-founder.webp';
+
+const featuresData = [
+  {
+    num: 1,
+    title: 'Actionable insights',
+    desc: 'Optimize your products, improve customer satisfaction and stay ahead of the competition with our product data analytics.',
+  },
+  {
+    num: 2,
+    title: 'Data-driven decisions',
+    desc: 'Make data-driven decisions with our product data analytics. Our AI-generated reports help you unlock insights hidden in your product data.',
+  },
+  {
+    num: 3,
+    title: 'Always affordable',
+    desc: 'Always affordable pricing that scales with your business. Get top-quality product data analytics services without hidden costs or unexpected fees.',
+  },
+];
+
+export {
+  LightLogo,
+  DarkLogo,
+  HeroImage,
+  LeftPattern,
+  RightPattern,
+  Founder,
+  featuresData,
+};
